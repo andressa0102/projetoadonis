2a948cfeacea5f42c7e17efd68b074cea33c5860
andressa0102 <andressas.as1235@gmail.com>
2022-09-30 10:28:02 -0300

    Mensagem

--- database/migrations/1661520155003_carros.ts
+++ database/migrations/1661520155003_carros.ts
@@ -7,8 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('nome')
-      table.
-      ('ano')
+      table.integer('ano')
       table.float('valor')
 
       
